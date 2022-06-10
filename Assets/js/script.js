d805be595acd5441bf878d981dac113bc77a51ed
Mariah <mariahjohnston217@outlook.com>
2022-06-10 19:22:33 -0400

    added onclick for search button, added function to store text from textarea to local storage, created more variables

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,9 @@
-//Step 1: create static selector variables
+//Step 1: create static selector variables  ✔️
 //Step 2: create addEventListener for search button
-//Step 3: grab input values from textbox
+//Step 3: grab input values from textbox and put in local storage
+//Step 4: unhide 5 day forecast cards
+//Step 5: add API
+//Step 6: create add event listener for quick select city buttons
 
 
 //use addeventlistener for search button to trigger function for search textarea
@@ -8,3 +11,31 @@
 //Variables:
 var searchCityEl = document.querySelector("#search-city");
 var searchBtn = document.querySelector("#search-button");
+
+var cityArrayBtns = ["austin", "chicago", "newyork", "orlando", "sanfransisco", "seattle", "denver", "atlanta"]
+
+var currentWeather = ["weather", "current-temp", "current-wind", "current-humidity", "current-index"]
+
+// function displaySearch ()
+
+
+//function to put text from textarea into local storage//
+function searchInput(){
+    document.getElementById("search-city");
+    var search = document.getElementById("search-city").value;
+    localStorage.setItem("search-city", search);
+    console.log(search);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
