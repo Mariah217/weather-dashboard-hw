bfebac54e074160cc5c4f39083aa26fe80e48586
Mariah <mariahjohnston217@outlook.com>
2022-06-09 23:13:26 -0400

    commented more of my css, added 5 day forecast

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,6 +2,9 @@
 //Step 2: create addEventListener for search button
 //Step 3: grab input values from textbox
 
+
+//use addeventlistener for search button to trigger function for search textarea
+
 //Variables:
 var searchCityEl = document.querySelector("#search-city");
 var searchBtn = document.querySelector("#search-button");
