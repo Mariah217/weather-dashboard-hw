049f9c8e24121b377b2acc5d96c5064d4e0a50d5
Mariah <mariahjohnston217@outlook.com>
2022-06-10 21:42:27 -0400

    added classes to my quick city select buttons, added hover to search button, created more variables in js, started function for city name and date and function for search input

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,9 @@
 //Step 2: onclick for search button ✔️
 //Step 3: grab input values from textbox and put in local storage ✔️
 //Step 4: unhide 5-day forecast cards
-//Step 5: add API
-//Step 6: create add event listener for quick select city buttons
-//Step 7: create fetch request for data
+//Step 5: add API ✔️
+//Step 6: create add event listener for quick select city buttons ✔️
+//Step 7: create fetch request for data and put on page 
 
 //api key
 var APIKey = "789d6254dc04a55a39b161980f745319";
@@ -12,6 +12,11 @@ var APIKey = "789d6254dc04a55a39b161980f745319";
 //Variables:
 var searchCityEl = document.querySelector("#search-city");
 var searchBtn = document.querySelector("#search-button");
+var userFormEl = document.querySelector("#user-form");
+var dateEL = document.querySelector(".card-title");
+var tempEl = document.querySelector("#temp");
+var windEl = document.querySelector("#wind");
+var humidityEl = document.querySelector("#humidity");
 var city;
 var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
@@ -25,10 +30,25 @@ function searchInput(){
     var search = document.getElementById("search-city").value;
     localStorage.setItem("search-city", search);
     console.log(search);
+    getApi();
+}
+
+//function for city name and date
+var formSubmit = function(event) {
+    event.preventDefault();
+    var citySearch = userFormEl.value;
+
+    if(citySearch){
+        getCityWeather(citySearch);
+
+        dateEL.textContent = ' ';
+        citySearch.value = ' ';
+    }
 }
 
 //api search
-fetch (apiUrl)
+function getApi (){
+    fetch (apiUrl)
     .then(function (response){
         return response.json();
     })
@@ -37,6 +57,7 @@ fetch (apiUrl)
        
     }
 })
+}
 
 //function to display search results under city name and date and cards. Unhide cards (use visible for bootstrap)
 // function displaySearch ()
@@ -47,6 +68,5 @@ fetch (apiUrl)
 
 
 
-
-
-
+//addEventListener for quick city select buttons
+cityArrayBtns.addEventListener('click', getApi);
