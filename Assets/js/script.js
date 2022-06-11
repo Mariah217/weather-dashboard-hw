8e0e9414b2aaf74b8b510d95638ed9b0efd214bc
Mariah <mariahjohnston217@outlook.com>
2022-06-10 20:03:24 -0400

    created var for api key and api url, started fetch request

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,23 @@
-//Step 1: create static selector variables  ✔️
-//Step 2: create addEventListener for search button
-//Step 3: grab input values from textbox and put in local storage
-//Step 4: unhide 5 day forecast cards
+//Step 1: create static selector variables ✔️
+//Step 2: onclick for search button ✔️
+//Step 3: grab input values from textbox and put in local storage ✔️
+//Step 4: unhide 5-day forecast cards
 //Step 5: add API
 //Step 6: create add event listener for quick select city buttons
+//Step 7: create fetch request for data
 
-
-//use addeventlistener for search button to trigger function for search textarea
+//api key
+var APIKey = "789d6254dc04a55a39b161980f745319";
 
 //Variables:
 var searchCityEl = document.querySelector("#search-city");
 var searchBtn = document.querySelector("#search-button");
+var city;
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 var cityArrayBtns = ["austin", "chicago", "newyork", "orlando", "sanfransisco", "seattle", "denver", "atlanta"]
 
-var currentWeather = ["weather", "current-temp", "current-wind", "current-humidity", "current-index"]
-
-// function displaySearch ()
-
+var currentWeatherArray = ["weather", "current-temp", "current-wind", "current-humidity", "current-index"]
 
 //function to put text from textarea into local storage//
 function searchInput(){
@@ -27,8 +27,19 @@ function searchInput(){
     console.log(search);
 }
 
+//api search
+fetch (apiUrl)
+    .then(function (response){
+        return response.json();
+    })
+.then (function (data){
+    for (let i = 0; index < data.length; index++) {
+       
+    }
+})
 
-
+//function to display search results under city name and date and cards. Unhide cards (use visible for bootstrap)
+// function displaySearch ()
 
 
 
